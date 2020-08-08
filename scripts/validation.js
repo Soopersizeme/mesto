bcb19e3b51984b2cf0aac030f484a014dafcc8b4
unknown <soopersizeme@ya.ru>
2020-08-08 18:48:50 +0300

    fix: validation form

--- scripts/validation.js
+++ scripts/validation.js
@@ -64,8 +64,8 @@ enableValidation({
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__save-button",
-  inactiveButtonClass: ".popup__save-button_disabled",
-  inputErrorClass: ".popup__input_type_error",
-  errorClass: ".popup__input-error_invisible"
+  inactiveButtonClass: "popup__button-disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error_invisible"
 });
 
