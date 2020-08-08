68954a121ff574ebde811034f2e6e3a81c7db411
unknown <soopersizeme@ya.ru>
2020-08-08 17:56:03 +0300

    feat: validation form

--- scripts/validation.js
+++ scripts/validation.js
@@ -59,12 +59,13 @@ const enableValidation = ({formSelector, inputSelector, submitButtonSelector, in
   });
 };
 
+
 enableValidation({
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__save-button",
   inactiveButtonClass: ".popup__save-button_disabled",
   inputErrorClass: ".popup__input_type_error",
-  errorClass: ".popup__input-error_visible",
+  errorClass: ".popup__input-error_invisible"
 });
 
